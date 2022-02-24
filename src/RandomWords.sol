7c297a27bc8e77a2571b1a24fc980c33dfeca25d
Evan Tan <tcyevan@gmail.com>
2022-02-24 22:23:22 +0800

    chore: move Strings into its own file

--- src/RandomWords.sol
+++ src/RandomWords.sol
@@ -3,54 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "base64/base64.sol";
-
-library Strings8 {
-    function toString(uint8 value) internal pure returns (string memory) {
-        // Inspired by OraclizeAPI's implementation - MIT licence
-        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
-
-        if (value == 0) {
-            return "0";
-        }
-        uint8 temp = value;
-        uint8 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint8(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
-}
-
-library Strings16 {
-    function toString(uint16 value) internal pure returns (string memory) {
-        // Inspired by OraclizeAPI's implementation - MIT licence
-        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
-
-        if (value == 0) {
-            return "0";
-        }
-        uint16 temp = value;
-        uint16 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint16(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
-}
+import "./Strings.sol";
 
 contract RandomWords is ERC721 {
     using Strings8 for uint8;
