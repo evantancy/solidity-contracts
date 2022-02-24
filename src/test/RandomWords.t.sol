dc483508492e01f87ce9c25dd21ed8b95c113a69
Evan Tan <tcyevan@gmail.com>
2022-02-24 21:54:30 +0800

    test: add tests and cheatcodes

--- src/test/RandomWords.t.sol
+++ src/test/RandomWords.t.sol
@@ -5,7 +5,17 @@ import "ds-test/test.sol";
 import "../RandomWords.sol";
 
 interface CheatCodes {
+    // Sets the *next* call's msg.sender to be the input address
     function prank(address) external;
+
+    // Sets all subsequent calls' msg.sender to be the input address until `stopPrank` is called
+    function startPrank(address) external;
+
+    // Sets all subsequent calls' msg.sender to be the input address until `stopPrank` is called, and the tx.origin to be the second input
+    function startPrank(address, address) external;
+
+    // Resets subsequent calls' msg.sender to be `address(this)`
+    function stopPrank() external;
 }
 
 contract RandomWordsTest is DSTest {
@@ -22,6 +32,7 @@ contract RandomWordsTest is DSTest {
     }
 
     function testFailMint0() public {
+        cheats.prank(USER);
         rwContract.mint(0);
     }
 
@@ -39,5 +50,30 @@ contract RandomWordsTest is DSTest {
         cheats.prank(USER);
         rwContract.mint(51);
     }
-}
 
+    function testMintMAX_HOLD() public {
+        uint16 MAX_TX = 50;
+        cheats.startPrank(USER);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(33);
+        cheats.stopPrank();
+    }
+
+    function testFailMintMAX_HOLD() public {
+        uint16 MAX_TX = 50;
+        cheats.startPrank(USER);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        rwContract.mint(MAX_TX);
+        cheats.stopPrank();
+    }
+}
