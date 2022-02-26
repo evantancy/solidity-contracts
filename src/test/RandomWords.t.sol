90ad25b52063568b503557c8b78ff8f00723f62c
Evan Tan <tcyevan@gmail.com>
2022-02-26 14:20:07 +0800

    refactor: delete unnecessary lines in tests

--- src/test/RandomWords.t.sol
+++ src/test/RandomWords.t.sol
@@ -4,26 +4,25 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "../RandomWords.sol";
 
-interface CheatCodes {
-    // Sets the *next* call's msg.sender to be the input address
+interface VM {
     function prank(address) external;
 
-    // Sets all subsequent calls' msg.sender to be the input address until `stopPrank` is called
     function startPrank(address) external;
 
-    // Resets subsequent calls' msg.sender to be `address(this)`
     function stopPrank() external;
 }
 
 contract RandomWordsTest is DSTest {
     RandomWords rwContract;
-    CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
-    address public USER = 0x5C2C4de7C947dEE988A4471D0270ECbaee92a961;
+    VM constant vm =
+        VM(address(bytes20(uint160(uint256(keccak256(("hevm cheat code")))))));
+    address who = 0x5C2C4de7C947dEE988A4471D0270ECbaee92a961;
     uint16 max_tx;
 
     function setUp() public {
         rwContract = new RandomWords();
         max_tx = rwContract.MAX_TX();
+        vm.startPrank(who);
     }
 
     function testInitialSupply() public {
@@ -31,31 +30,26 @@ contract RandomWordsTest is DSTest {
     }
 
     function testFailMint0() public {
-        cheats.prank(USER);
         rwContract.mint(0);
     }
 
     function testMint1() public {
-        cheats.prank(USER);
         rwContract.mint(1);
         assertEq(rwContract.currentSupply(), 1);
-        assertEq(rwContract.balanceOf(USER), 1);
+        assertEq(rwContract.balanceOf(who), 1);
     }
 
     function testMintMAX_TX() public {
-        cheats.prank(USER);
         rwContract.mint(max_tx);
         assertEq(rwContract.currentSupply(), max_tx);
     }
 
     function testFailMintMAX_TX() public {
-        cheats.prank(USER);
         rwContract.mint(max_tx + 1);
     }
 
     function testMintMAX_HOLD() public {
         uint16 quantity = rwContract.MAX_HOLD();
-        cheats.startPrank(USER);
         while (quantity > 0) {
             if (quantity >= max_tx) {
                 rwContract.mint(max_tx);
@@ -65,15 +59,12 @@ contract RandomWordsTest is DSTest {
                 quantity -= quantity;
             }
         }
-        cheats.stopPrank();
         assertEq(rwContract.currentSupply(), rwContract.MAX_HOLD());
     }
 
     function testFailMintMAX_HOLD() public {
-        cheats.startPrank(USER);
         for (uint8 i = 0; i < 8; ++i) {
             rwContract.mint(max_tx);
         }
-        cheats.stopPrank();
     }
 }
